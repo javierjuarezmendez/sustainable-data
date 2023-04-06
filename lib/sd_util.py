9bebe4e80dcd15a6b19d28cae99bffce8157ed9a
Javier Juarez <javierjuarezmendez@gmail.com>
2023-04-06 12:55:25 -0400

    Updated library properly.

---
+++ lib/sd_util.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# function: save()
+# desc:     saves a pandas dataframe to a basic csv format
+# input:    pandas dataframe
+# output:   none
+def save(df):
+    print("Saving to .csv format ...")
+    df.to_csv(index=False)
+
+def read(fname):
+    df = pd.read_csv(fname)
+    return df
+
+def graph(df):
+    df.plot()
+    plt.xlabel('xlabel')
+    plt.ylabel('ylabel')
+    plt.title('title')
+    plt.show()
+
+
