58d0d4817da8ef9b4ac91c1a704c646ad7742160
Javier Juarez <javierjuarezmendez@gmail.com>
2023-04-13 16:55:54 -0400

    Updated with new stuff

--- lib/sd_util.py
+++ lib/sd_util.py
@@ -10,10 +10,27 @@ def save(df):
     print("Saving to .csv format ...")
     df.to_csv(index=False)
 
+# function: read()
+# desc:     
+# input:
+# ouptut:
 def read(fname):
     df = pd.read_csv(fname)
     return df
 
+# function: sd_read()
+# desc:     
+# input:
+# ouptut:
+# def sd_write(fname, df):
+
+#    break
+
+
+# function: graph()
+# desc:     takes a time over temp data in a pandas DataFrame and graphs it 
+# input:    pandas DataFrame with time and temp
+# ouptut:   no return, plots a graph and saves the figure
 def graph(df):
     df.plot()
     plt.xlabel('Time')
@@ -21,4 +38,25 @@ def graph(df):
     plt.title('Temp over Time')
     plt.savefig('graph.png')
 
-def temp_save(fname, time, ):
+# function: live_graph()
+# desc:     
+# input:
+# ouptut:
+def live_graph(df):
+    df2 = df[0:0]
+
+    plt.ion()
+    fig, ax = plt.subplots()
+
+    i = 0
+    while i < len(df):
+        df2 = pd.concat([df2, df[i:i+1]])
+        ax.clear()
+        df2.plot(x="x", y="y", ax=ax)
+        plt.draw()
+        # plt.pause(0.2)
+        i+=1
+
+    plt.show() 
+
+
--- tests.py
+++ tests.py
@@ -1,21 +1,93 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import lib.sd_util
+import os
+from pandasgui import show
+import sys
+from PyQt5.QtCore import QSize, Qt
+import PyQt5.QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QSlider, QVBoxLayout, QGridLayout, QGroupBox, QLabel
 
 # constants
 FNAME = 'tests/tests.csv'
 
 # main entry
 def main():
+    test1()
+    # test2()
+    test3()
+
+def test1():
     df = lib.sd_util.read(FNAME)
     lib.sd_util.graph(df)
-    # print(df.head())
-    # df.plot()
-    # plt.savefig('tests/tests.png')
+
+def test2():
+    df = pd.DataFrame({"time":None, "temperature":None},index=[0])
+    print(df)
+
+    plt.ion
+    fig, ax = plt.subplots()
+
+    gui = show(df, settings = {'block': False})
+
+    i = 0
+    while i < 100:
+        ax.clear()
+        j = np.random.choice(list(range(60,80)))
+        df2 = pd.DataFrame({"time":i,"temperature":j}, index=[0])
+        df = pd.concat([df,df2])
+        pgdf = gui.store.data['df']
+        pgdf.refresh_ui()
+        print(df)
+        # now live animate
+        df.plot(x="time",y="temperature",ax=ax)
+        plt.draw()
+        plt.pause(1)
+        i+=1
+
+    plt.show()
+
+def test3():
     
-def bacnet_test():
+    class MainWindow(QWidget):
+        def __init__(self):
+            
+            super().__init__()
+
+            grid = QGridLayout()
+            grid.addWidget(self.createSlider(),0, 0)
+            self.setLayout(grid)
+            self.setWindowTitle("Sustainable Data")
+            self.resize(500, 500)
+
+        def createSlider(self):
+            groupBox = QGroupBox("Max Temp.")
+
+            slider = QSlider(Qt.Horizontal)
+
+            slider.label = QLabel(alignment=Qt.AlignCenter)
+            label_minimum = QLabel(alignment=Qt.AlignLeft)
+            slider.minimumChanged.connect(label_minimum.setNum)
+            sjlider.setMinimum(0)
+            slider.setMaximum(100)
+            slider.setValue(77)
+            slider.setTickPostition(QSlider.TicksBothSides)
+            slider.setTickInterval(1)
+
+            vbox = QVBoxLayout()
+            vbox.addWidget(slider)
+            groupBox.setLayout(vbox)
+
+            return groupBox
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    app.exec()
 
 
-# running main
+# running main 
 if __name__ == '__main__':
     main()
